ad880c485ba836359679f966b9e8219ed2b748a0
Giovanne Almeida <giovanne.almeida91@gmail.com>
2016-11-29 12:44:35 -0200

    first commit

---
+++ app/src/main/java/br/com/versalius/akijob/MainActivity.java
@@ -0,0 +1,115 @@
+package br.com.versalius.akijob;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import br.com.versalius.akijob.activities.LoginActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText etCity, etService;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setLogo(R.drawable.toolbar_logo);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+
+        etService = (EditText) findViewById(R.id.etService);
+        etCity = (EditText) findViewById(R.id.etCity);
+
+        Button btSearch = (Button) findViewById(R.id.btSearch);
+        btSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                submitForm();
+            }
+        });
+    }
+
+    /**
+     * Validating form
+     */
+    private void submitForm() {
+        if (!validateService()) {
+            return;
+        }
+
+        if (!validateCity()) {
+            return;
+        }
+
+        Toast.makeText(getApplicationContext(), "Buscando", Toast.LENGTH_SHORT).show();
+    }
+
+    private boolean validateService() {
+        if (etService.getText().toString().trim().isEmpty()) {
+            etService.setError(getString(R.string.err_msg_service));
+            requestFocus(etService);
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean validateCity() {
+        if (etCity.getText().toString().trim().isEmpty()) {
+            etCity.setError(getString(R.string.err_msg_city));
+            requestFocus(etCity);
+            return false;
+        }
+
+        return true;
+    }
+
+    private void requestFocus(View view) {
+        if (view.requestFocus()) {
+            getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_login) {
+            startActivity(new Intent(this, LoginActivity.class));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/br/com/versalius/akijob/activities/LoginActivity.java
@@ -0,0 +1,38 @@
+package br.com.versalius.akijob.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import br.com.versalius.akijob.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        getSupportActionBar().setTitle("Voltar");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        if (menuItem.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(menuItem);
+    }
+}
